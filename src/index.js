c28ebb59ac986e4da0537e41b5787e26db8cd15e
kerubo <mongaremetrine5@gmail.com>
2025-12-18 23:27:30 +0300

    html forms

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const input = document.getElementById('new-task-description');
+    const taskText = input.value;
+
+    if (taskText.trim() === '') return;
+
+    const taskList = document.getElementById('tasks');
+    const li = document.createElement('li');
+    li.textContent = taskText;
+    taskList.appendChild(li);
+
+     input.value = '';
+  });
 });
